9e8fbd50422e29cbd8cfb42830df6b5f712f0ec3
alameen.kh <alameen.kh@charmboard.com>
2020-06-19 09:39:37 +0530

    added thee logo and section for the paragraph

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,32 +1,30 @@
 import React, { Component } from 'react';
 import {Menu, Container} from 'semantic-ui-react'
 import '../App.css'
-import { TimelineLite } from "gsap";
-import logo from "../logo.svg";
-// export default function Home (){
-// 	constructor(props) {
-//     	super(props);
-//     	this.intro = React.createRef(); 
-//   	}
-// 	return (
-// 		<div class="home">
-// 		<p>home</p>
-// 		</div>
-// 	)
-// import "./App.css";
+import { Power2,TimelineLite } from "gsap";
+import logo from '../logo.png';
 
 class Home extends Component {
+
   constructor(props) {
+  	console.log("starts",logo);
     super(props);
     this.intro = React.createRef(); // shiny new React 16.3 ref API!
     this.logo = React.createRef();
+    this.content = React.createRef();
   }
 componentDidMount() {
     const tl = new TimelineLite();
-    
     tl
       .to(this.intro.current, 4.5, { opacity: 1, delay: 1 })
-      .to(this.intro.current, 1.5, { opacity: 0 });
+      .to(this.intro.current, 1.5, { opacity: 0 })
+      .set(this.logo.current, { opacity: 1, scale: 2.75 })
+      .to(this.logo.current, 8, { 
+        scale: 0.05, 
+        ease: Power2.easeOut
+      })
+      .to(this.logo.current, 1.5, { opacity: 0 }, "-=1.5")
+      .to(this.content.current, 200, { top: "-170%" });
   }
 
   render() {
@@ -38,8 +36,15 @@ componentDidMount() {
           </p>
         </section>
         <section className="logo" ref={this.logo}>
-          <img src={logo} alt="Code Wars logo" />
+          <img src={logo} alt="Star Wars logo" />
         </section>
+        <section className="crawl">
+		  <div className="mainContent" ref={this.content}>
+		  <p>
+		   	To get the details of People and Planet on the star wars Click on the menu on the navbars.
+		  </p> 	
+		  </div>
+		</section>
       </div>
     );
   }
